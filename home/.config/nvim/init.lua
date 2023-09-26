e3cda0f3e91be2d7c58f814378338b441a27896b
Orchid <137848811+mariospicross@users.noreply.github.com>
2023-09-26 19:36:19 -0400

    Create init.lua

---
+++ home/.config/nvim/init.lua
@@ -0,0 +1,11 @@
+require("core.keymaps")
+require("core.plugins")
+require("core.plugin_config")
+require("core.options")
+
+-- open nvim-tree on startup
+local function open_nvim_tree()
+    require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
