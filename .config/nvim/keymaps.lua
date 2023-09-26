f275ba69993aba0a104969ae6639de25c7c15243
Orchid <137848811+mariospicross@users.noreply.github.com>
2023-09-26 19:35:52 -0400

    Delete .config/nvim directory

--- .config/nvim/keymaps.lua
+++
@@ -1,6 +0,0 @@
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
-
-vim.keymap.set('n', '<c-down>', '<c-w><c-j>')
-vim.keymap.set('n', '<c-up>', '<c-w><c-k>')
-vim.keymap.set('n', '<c-right>', '<c-w><c-l>')
-vim.keymap.set('n', '<c-left>', '<c-w><c-h>')
--- .config/nvim/trouble.lua
+++
@@ -1,5 +0,0 @@
-require("trouble").setup{
-  height = 3,
-  auto_open = true,
-  auto_close = false,
-}
